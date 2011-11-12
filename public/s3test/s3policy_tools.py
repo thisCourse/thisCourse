e07bfc022b0b502b48721606f44cfdb408feb75e
Micah Bregman <micah@gustav.local>
2011-11-12 12:19:36 -0800

    Working client side s3 upload. That was easy!
    json policy with max file size, etc. has to be run through the python
    script to generate an encoded and a signed policy. There is probably js
    code for this somewhere, but I used the python without issue.
    
    Used info here:
    http://aws.amazon.com/articles/1434

---
+++ public/s3test/s3policy_tools.py
@@ -0,0 +1,25 @@
+import base64
+import hmac, sha
+
+def create_policy(json):
+    return base64.b64encode(json)
+
+def sign_policy(policy):
+    aws_secret_key = 'MgZOrTRimfNRbX2BH0ErwdnjsvNAsxSRc2B+3sT2'
+    return base64.b64encode(hmac.new(aws_secret_key, policy, sha).digest())
+
+if __name__ == '__main__':
+    policy_json = """{"expiration": "2012-01-01T00:00:00Z",
+      "conditions": [ 
+        {"bucket": "thiscourse_uploads"}, 
+        ["starts-with", "$key", "uploads/"],
+        {"acl": "private"},
+        {"success_action_redirect": "http://localhost/"},
+        ["starts-with", "$Content-Type", ""],
+        ["content-length-range", 0, 1048576]
+      ]
+    }"""
+    enc_policy =  create_policy(policy_json)
+    print enc_policy
+    print sign_policy(enc_policy)
+
