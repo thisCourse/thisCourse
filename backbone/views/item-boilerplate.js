a6d43e114d6f5bbc8172d8f93ddcccec384fff86
Jamie Alexandre <jamalex@wa.(none)>
2011-11-17 14:50:20 -0800

    Tweaks to gallery.

--- backbone/views/item-boilerplate.js
+++ backbone/views/item-boilerplate.js
@@ -1,42 +1,46 @@
-var type = "ITEMTYPE"
+(function() {
 
-ItemViews[type] = ItemView.extend({
-    base: ItemView.prototype,
-    render: function() {
-        this.renderTemplate()
-        this.updateWidth()
-        return this
-    },
-    events: function() {
-        return _.extend({
-            //"dblclick": "dblclick"
-        }, this.base.events)
-    },
-    initialize: function() {
-        this.base.initialize.apply(this, arguments)
-    },
-    close: function() {
-        this.base.close.apply(this, arguments)
-    }
-})
+    var type = "ITEMTYPE"
+    
+    ItemViews[type] = ItemView.extend({
+        base: ItemView.prototype,
+        render: function() {
+            var self = this
+            this.base.render.apply(this, arguments)
+            return this
+        },
+        events: function() {
+            return _.extend({
+                //"dblclick": "dblclick"
+            }, this.base.events())
+        },
+        initialize: function() {
+            this.base.initialize.apply(this, arguments)
+        },
+        close: function() {
+            this.base.close.apply(this, arguments)
+        }
+    })
+    
+    var baseview = ItemEditInlineView 
+    
+    ItemEditViews[type] = baseview.extend({
+        minwidth: 6,
+        render: function() {
+            baseview.prototype.render.apply(this, arguments)
+            return this
+        },
+        events: function() {
+            return _.extend({
+                //"dblclick": "dblclick"
+            }, baseview.prototype.events())
+        },
+        initialize: function() {
+            baseview.prototype.initialize.apply(this, arguments)
+        },
+        close: function() {
+            baseview.prototype.close.apply(this, arguments)
+        }
+    })
 
-var EditorView = ItemEditInlineView
-
-ItemEditViews[type] = EditorView.extend({
-    base: EditorView.prototype,
-    render: function() {
-        this.base.render.apply(this, arguments)
-        return this
-    },
-    events: function() {
-        return _.extend({
-            //"dblclick": "dblclick"
-        }, this.base.events)
-    },
-    initialize: function() {
-        this.base.initialize.apply(this, arguments)
-    },
-    close: function() {
-        this.base.close.apply(this, arguments)
-    }
-})
+})()
