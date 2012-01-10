2ce83b18b4da8ff4388c5bcb8e745d0b1749b2ed
Jamie Alexandre <jamalex@wa.(none)>
2012-01-10 12:45:33 -0800

    GA addition.

--- backbone/router.js
+++ backbone/router.js
@@ -224,6 +224,7 @@ function ga_initialize() {
 	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	var s = document.getElementsByTagName('script')[0];
 	s.parentNode.insertBefore(ga, s);
+	ga_track_pageview(window.location.pathname)
 }
 
 function ga_track_pageview(url) {
