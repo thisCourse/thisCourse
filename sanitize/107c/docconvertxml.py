80a5569d2529b1c24a0c19ae3b0204c6b171342f
rtibbles <rtibbles@ucsd.edu>
2012-03-31 14:23:16 -0700

    Nugget collecting appears to be working. Probe collecting is borken.

--- sanitize/107c/docconvertxml.py
+++ sanitize/107c/docconvertxml.py
@@ -32,36 +32,102 @@ def htmlfromxml(filename,j=0):
     
     glossed = False
     
+    glosstitle = None
+    
+    xmljunk = ['14:hiddenLine>','14:hiddenFill>'] #hack to get rid of residual junk from xml processing
+    
     for i,x in enumerate(soup.findAll(text=alphanum)):
-        gloss = 0
-        glossy = 0
-        if i+j:
-            html = '<p style="'
+        if x in xmljunk:
+            None
         else:
-            html = '<h3 style="'
-        for y in x.parent.previous_siblings:
-            for z in y.findAll(lambda tag: tag.has_key('val') or tag.has_key('b')):
-                if z.has_key('b'):
-                    if z['b']=='1':
-                        html+='font-weight:bold;'
-                if z.has_key('val'):
-                    html+='color:#'+z['val']
-                    if z['val']=='0000FF':
-                        gloss = 1
-        if i+j:
-            htmlout+=html+'">'+x+'</p>'
-        else:
-            htmlout+=html+'">'+x+'</h3>'
-            glossy = gloss
-        if glossy:
-            glossed = True
-    return htmlout, glossed
+            gloss = 0
+            glossy = 0
+            if i+j:
+                html = '<p style="'
+            else:
+                html = '<h3 style="'
+            for y in x.parent.previous_siblings:
+                for z in y.findAll(lambda tag: tag.has_key('val') or tag.has_key('b')):
+                    if z.has_key('b'):
+                        if z['b']=='1':
+                            html+='font-weight:bold;'
+                    if z.has_key('val'):
+                        html+='color:#'+z['val']
+                        if z['val']=='0000FF':
+                            gloss = 1
+            if i+j:
+                htmlout+=html+'">'+x+'</p>'
+            else:
+                htmlout+=html+'">'+x+'</h3>'
+                glossy = gloss
+                glosstitle = x
+            if glossy:
+                glossed = True
+    return htmlout, glossed, glosstitle
 
 
-slidesnotes = {}
+slides = {}
+glossaryslides = {}
 
 for slide in glob.glob('*/slides/*.xml'):
-    markup, glossary = htmlfromxml(slide)
-    slidedict = {'html':markup,'glossary':glossary}
+    markup, glossary,glossname = htmlfromxml(slide)
     slidekey = slide.split('/')[0]+slide.split('.')[0].split('/')[2]
-    slidesnotes[slidekey] = slidedict
+    slidedict = {'html':markup}
+    if glossary:
+        glossaryslides[glossname]=slidedict
+    slides[slidekey] = slidedict
+
+
+lecnug = re.compile('(?P<lecture>L[0-9]+)\.(?P<cluster>C[0-9]+)\.(?P<nugget>N[0-9]+)\.(?P<subpage>[0-9]+)')
+
+questsplit = 'L[0-9]+\.C[0-9]+\.N[0-9]+\.Q[0-9]+'
+
+question = re.compile(questsplit)
+
+paragraph = re.compile('<p[^<]*</p>')
+
+questioncollection = {}
+
+nuggetcollection = {}
+    
+for slide in glob.glob('*/notesSlides/*.xml'):
+    markup, glossary, glossname = htmlfromxml(slide,j=1)
+    questlist = re.split(questsplit,markup)
+    finding = lecnug.search(questlist[0])
+    slidekey = slide.split('/')[0]+slide.split('.')[0].split('/')[2].replace('notesS','s')
+    
+    if finding:
+        tags = []
+        lecture,cluster,nugget,subpage = finding.groups()
+        nuggetkey = lecture+cluster+nugget
+        nuggetdict = {}
+        slidepage = 'page'+subpage
+        nuggetdict[slidepage] = slides[slidekey]
+        tagsection = lecnug.sub('',questlist[0])
+        tagsoup = BeautifulSoup(tagsection)
+        for tag in tagsoup.text.split(','):
+            tags.append(tag)
+        tags = tags + [lecture,cluster,nugget]
+        nuggetdict['tags'] = tags
+        if len(questlist)>1:
+            nuggetqs = []
+            for probe in questlist[1:]:
+                if question.search(probe):
+                    paras = paragraph.findall(probe)
+                    answers = []
+                    probequestion = paras[0]
+                    for para in paras[1:]:
+                        answer = {}
+                        if para.find('font-weight'):
+                            answer['correct'] = True
+                        answer['text'] = para
+                        answers.append(answer)
+                    probedict = {'questiontext':probequestion,'answers':answers}
+                    probekey = question.search(probe).group().replace('.','')
+                    questioncollection[probekey] = probedict
+                    nuggetqs.append(probekey)
+                
+        if nuggetcollection.has_key(nuggetkey):
+            nuggetcollection[nuggetkey] = dict(nuggetcollection[nuggetkey].items() + nuggetdict.items())
+        else:
+            nuggetcollection[nuggetkey] = nuggetdict
