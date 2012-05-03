9303605dd3447bf3dde92bf7eecc20a769c1c09a
Jamie Alexandre <jamalex@gmail.com>
2012-05-03 13:22:11 -0700

    Updated analytics backend to allow for pruning ("unclaiming"). Updated build.sh to copy jquery-ui css images to a folder where they'll be usable.

--- require/build.sh
+++ require/build.sh
@@ -7,3 +7,4 @@ node tools/r.js -o build.js
 printf "\nvar git_revision = \"`git log --pretty=format:'%h' -n 1`\";\n\n" >> build/engine.js
 cat build/libs/underscore.js build/libs/requirejs/require.js build/bootloader.js >> build/engine.js
 cat src/libs/jquery/jquery-ui-1.8.16.custom.css >> build/styles.css
+cp -r src/libs/jquery/images build
